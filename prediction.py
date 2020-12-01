310c9208064a209b50ce4f7cce180017cba3ff16
Vatsal Verma <55058681+vatsalcode@users.noreply.github.com>
2020-12-01 18:13:05 +0530

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Vatsal verma
+"""
+
+import pickle
+
+#doc_new = ['obama is running for president in 2016']
+
+var = input("Please enter the news text you want to verify: ")
+print("You entered: " + str(var))
+
+
+#function to run for prediction
+def detecting_fake_news(var):    
+#retrieving the best model for prediction call
+    load_model = pickle.load(open('final_model.sav', 'rb'))
+    prediction = load_model.predict([var])
+    prob = load_model.predict_proba([var])
+
+    return (print("The given statement is ",prediction[0]),
+        print("The truth probability score is ",prob[0][1]))
+
+
+if __name__ == '__main__':
+    detecting_fake_news(var)
